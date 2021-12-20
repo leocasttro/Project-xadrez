490c114d9a021b56cb19a4a2f1ece8ce121c0c4d
Leonardo de Castro <leocasttro01@icloud.com>
2021-12-20 14:26:14 -0300

    Criada a classe posicao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace ExercicioXadrez
 {
@@ -6,7 +7,10 @@ namespace ExercicioXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
