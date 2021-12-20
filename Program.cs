f3d298360ca97a5a48888a5eb068148e7b3ec35c
Leonardo de Castro <leocasttro01@icloud.com>
2021-12-20 13:08:16 -0300

    Projeto de xadrez iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
